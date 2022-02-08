3ad737b15c8f3fb5c8b937d3de72b546c7733472
Mie Louise Lauridsen <mie-lauridsen@live.dk>
2022-02-08 12:16:07 +0100

    Exercise math started
    Signed-off-by: Mie Louise Lauridsen <mie-lauridsen@live.dk>

---
+++ exercises/math/main.cs
@@ -0,0 +1,15 @@
+using static System.Math;
+class math {
+	static void Main(){
+		double sqrt2=Sqrt(2.0);
+		System.Console.Write($"sqrt(2) = {sqrt2}\n");
+		System.Console.Write($"sqrt2*sqrt2 = {sqrt2*sqrt2}\n");
+		
+		double exppi=Exp(PI);
+		System.Console.Write($"exp(pi) = {exppi}\n");
+
+		double pie=Pow(PI,E);
+		System.Console.Write($"pi to the power of e = {pie}\n");
+}
+
+}
