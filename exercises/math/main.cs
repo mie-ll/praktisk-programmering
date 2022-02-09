da84f50efc61f38beda5a9abe56e7f881be3f598
Mie Louise Lauridsen <mie-lauridsen@live.dk>
2022-02-09 09:11:54 +0100

    Exercise math corrected
    Signed-off-by: Mie Louise Lauridsen <mie-lauridsen@live.dk>

--- exercises/math/main.cs
+++ exercises/math/main.cs
@@ -1,5 +1,5 @@
 using static System.Math;
-class math {
+public static class math {
 	static void Main(){
 		double sqrt2=Sqrt(2.0);
 		System.Console.Write($"sqrt(2) = {sqrt2}\n");
@@ -10,6 +10,6 @@ class math {
 
 		double pie=Pow(PI,E);
 		System.Console.Write($"pi to the power of e = {pie}\n");
-}
+	}
 
 }
