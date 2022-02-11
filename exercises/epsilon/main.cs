130fb433457b8a0cb00f520d977ce5c33ae55cc8
Mie Louise Lauridsen <mie-lauridsen@live.dk>
2022-02-11 15:51:34 +0100

    exercise epsilon
    Signed-off-by: Mie Louise Lauridsen <mie-lauridsen@live.dk>

---
+++ exercises/epsilon/main.cs
@@ -0,0 +1,13 @@
+using System; //name space
+using static System.Concole; //console
+using static System.Math;
+
+class epsilon{
+	
+	private static void maxint(){
+		int i=1;
+		while(i+1>i) {i++;}
+		Write("my max int = {0}\n",i);
+		}
+
+}
