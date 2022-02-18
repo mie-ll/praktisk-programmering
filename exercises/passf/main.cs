65c971ba20e84e7f34e420b247d32504b1d8b6ac
Mie Louise Lauridsen <mie-lauridsen@live.dk>
2022-02-18 15:51:20 +0100

    Exercise passf: started
    Signed-off-by: Mie Louise Lauridsen <mie-lauridsen@live.dk>

---
+++ exercises/passf/main.cs
@@ -0,0 +1,8 @@
+
+public static class main{
+	public static void Main(){
+		//Skal skal bruge make_table(sin(k*x)) og skrive outputtet ud....
+	}
+
+
+}
---
+++ exercises/passf/passf.cs
@@ -0,0 +1,6 @@
+
+public static class table{
+	public static void make_table(Func<double,double> f, double a, double b, double dx){
+		for(double x=a;x<=b;x+=dx)WriteLine($"{x} {f(x)}");
+		}
+}
