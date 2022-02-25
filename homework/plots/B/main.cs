0dec6e696857f5f0b6315f7045055902501075b6
Mie Louise Lauridsen <mie-lauridsen@live.dk>
2022-02-25 16:02:08 +0100

    Homework plots: A done, B started
    Signed-off-by: Mie Louise Lauridsen <mie-lauridsen@live.dk>

---
+++ homework/plots/B/main.cs
@@ -0,0 +1,18 @@
+using System;
+using static System.Console;
+using static System.Math;
+
+class main{
+	public static double gamma(double x){
+	///single precision gamma function (Gergo Nemes, from Wikipedia)
+		if(x<0)return PI/Sin(PI*x)/gamma(1-x);
+		if(x<9)return gamma(x+1)/x;
+		double lngamma=x*Log(x+1/(12*x-1/x/10))-x+Log(2*PI/x)/2;
+		return Exp(lngamma);
+	}
+
+	plublic static void Main(){
+		for(double x=0.1;x<=5;x+=1.0/8)
+			WriteLine($"{x} {gamma(x)}");
+	}
+}       
