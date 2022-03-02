7dc3b68798e78d381d8a9a42538ce4ac5212c747
Mie Louise Lauridsen <mie-lauridsen@live.dk>
2022-03-02 11:29:08 +0100

    Homework plots: part B changed and done, and part C done
    Signed-off-by: Mie Louise Lauridsen <mie-lauridsen@live.dk>

--- homework/plots/C/main.cs
+++ homework/plots/C/main.cs
@@ -6,15 +6,17 @@ class main{
 	
 	public static complex G(complex z){
 		if(cmath.abs(z)<0) return PI/cmath.sin(PI*z)/G(1-z);
-		if(cmath.abs(z)<9) return G(z+1)/x;
+		if(cmath.abs(z)<9) return G(z+1)/z;
 		complex lngamma = z*cmath.log(z+1/(12*z-1/z/10))-z+cmath.log(2*PI/z)/2;
 		return cmath.exp(lngamma);
 	}
 
 	public static void Main(){
-		for(double x = -4; x <= 4; x += 1.0/8){
-			for(double y = -4; y <= 4; y += 1.0/8){
-				\\.....
+		for(double x = -5.1; x <= 5.1; x += 1.0/32){
+			for(double y = -5.1; y <= 5.1; y += 1.0/32){
+				complex z = new complex(x, y);
+				complex gam = G(z);
+				WriteLine($"{z.Re} {z.Im} {cmath.abs(gam)}");
 			}
 		}
 		
