0dc4589a43f8f27df970ffa8e614a839ab36588c
Mie Louise Lauridsen <mie-lauridsen@live.dk>
2022-04-08 15:57:37 +0200

    Homework quadratures: part A done and part B started
    Signed-off-by: Mie Louise Lauridsen <mie-lauridsen@live.dk>

---
+++ homework/quadratures/A/integration.cs
@@ -0,0 +1,32 @@
+using System;
+using static System.Console;
+using static System.Math;
+using static System.Double;
+
+public class integration{
+	
+	public static double integrate(Func<double,double> f,
+		double a, double b,
+		double delta=0.001, double epsilon=0.001,
+		double f2=NaN, double f3=NaN){
+		
+		double h = b-a;
+		if(IsNaN(f2)){
+			f2 = f(a+2*h/6);
+			f3 = f(a+4*h/6);
+		}
+		double f1 = f(a+h/6);
+		double f4 = f(a+5*h/6);
+		double Q = (2*f1+f2+f3+2*f4)/6*(b-a);
+		double q = (f1+f2+f3+f4)/4*(b-a);
+		double err = Abs(Q-q);
+		if(err <= Max(delta, epsilon*Abs(Q))){
+			return Q;
+		} else {
+			return integrate(f,a,(a+b)/2,delta/Sqrt(2),epsilon,f1,f2) + 
+           		       integrate(f,(a+b)/2,b,delta/Sqrt(2),epsilon,f3,f4);
+		}
+	}
+
+
+}//class
---
+++ homework/quadratures/A/main.cs
@@ -0,0 +1,30 @@
+using System;
+using static System.Console;
+using static System.Math;
+
+public class main{
+	public static void Main(){
+		WriteLine($"Test of integration:");
+		double a = 0.0;
+		double b = 1.0;
+		
+		Func<double,double> f1 = x => Sqrt(x);
+		double result1 = integration.integrate(f1, a, b);
+		WriteLine($"Integration of sqrt(x) from 0 to 1 = {result1}");
+	
+		Func<double,double> f2 = x => 1.0/Sqrt(x);
+		double result2 = integration.integrate(f2, a, b);
+		WriteLine($"Integration of 1/sqrt(x) from 0 to 1 = {result2}");
+		
+		Func<double,double> f3 = x => 4.0*Sqrt(1.0-Pow(x,2));
+		double result3 = integration.integrate(f3, a, b);
+		WriteLine($"Integration of 4*sqrt(1-x^2) from 0 to 1 = {result3}");
+
+		Func<double,double> f4 = x => Log(x)/Sqrt(x);
+		double result4 = integration.integrate(f4, a, b);
+		WriteLine($"Integration of ln(x)/sqrt(x) from 0 to 1 = {result4}");
+
+	
+	}//Main
+
+}//class
