f3262aa2121f096a5090ca64ec4a11c2894442ed
Mie Louise Lauridsen <mie-lauridsen@live.dk>
2022-04-22 11:08:20 +0200

    Homework roots: started
    Signed-off-by: Mie Louise Lauridsen <mie-lauridsen@live.dk>

---
+++ homework/roots/A/roots.cs
@@ -0,0 +1,16 @@
+using System;
+using static System.Console;
+using static System.Math;
+
+public class roots{
+	public static vector newton(Func<vector,vector> f, vector x0, double epsilon=1e-2){
+		dx = ??
+		double lambda = 1.0;
+		while(f(x+lambda*dx).norm > (1-lambda/2)*f(x).norm){
+			lambda = lambda/2;
+		}
+		
+		
+	}
+
+}//class
