1e7df2c2f67d5d9e2984f510a7193989556fe311
Mie Louise Lauridsen <mie-lauridsen@live.dk>
2022-04-25 11:01:03 +0200

    Homework roots:....
    Signed-off-by: Mie Louise Lauridsen <mie-lauridsen@live.dk>

--- homework/roots/A/roots.cs
+++ homework/roots/A/roots.cs
@@ -4,13 +4,51 @@ using static System.Math;
 
 public class roots{
 	public static vector newton(Func<vector,vector> f, vector x0, double epsilon=1e-2){
-		dx = ??
-		double lambda = 1.0;
-		while(f(x+lambda*dx).norm > (1-lambda/2)*f(x).norm){
-			lambda = lambda/2;
+		vector x = x0.copy();
+		int n = x.size;
+		matrix J = new matrix(n,n); //calculating the Jacobian matrix J
+		double delta = 0;
+		vector fy = new vector(n);
+		vector y = new vector(n);
+		vector fx = new vector(n);
+
+		bool run1 = true;
+		while(run1){
+			fx = f(x);
+			for(int i=0; i<n; i++){
+				delta = Abs(x[i])*Pow(2,-23);
+				x[i] += delta;
+				for(int j=0; j<n; j++){
+					J[i,j] = (f(x)[i]-fx[i])/delta;
+				}
+				x[i] -= delta;
+			}
+		matrix R = new matrix(n,n);
+		matrix Q = J.copy();
+		lineq.QRGSdecomp(Q,R);
+		vector Dx = lineq.QRGSsolve(Q,R,-fx); // solving JDx=-fx, newtons step
+		
+		double lambda = 2.0;
+
+		bool run2 = true; //While(||f(x+lambda*Dx)|| < (1-lambda/2)*||f(x)|| )
+		while(run2){
+			lambda /= 2; //λ←λ/2
+			y = x+lambda*Dx; //x ← x + λ∆x
+			fy = f(y);
+			if(fy.norm() < (1-lambda/2.0)*fx.norm() || lambda < 1.0/64){
+				run2 = false;
+			}
+		}//run2
+		
+		x = y;
+		fx = fy;
+		if(Dx.norm() < delta || fx.norm() < epsilon){
+			run1 = false;
 		}
+		}//run1
 		
+		return x;
 		
-	}
+	}//newton
 
 }//class
