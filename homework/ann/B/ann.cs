47f801a86a8868443f9e6014b87ed41a7631d2bb
Mie Louise Lauridsen <mie-lauridsen@live.dk>
2022-05-17 13:10:08 +0200

    Homework neural netwrok: part B done
    Signed-off-by: Mie Louise Lauridsen <mie-lauridsen@live.dk>

---
+++ homework/ann/B/ann.cs
@@ -0,0 +1,73 @@
+using System;
+using static System.Console;
+using static System.Math;
+
+public class ann{
+	public int n; //number of hidden neurons
+	public Func<double,double> f; //activation function
+	public Func<double,double> f_diff;
+	public Func<double,double> f_int;
+	public vector p; //network parameters
+
+	public ann(int n, Func<double,double> f, Func<double,double> f_diff, Func<double,double> f_int){ //constructor
+		this.n = n;
+		this.f = f;
+		this.f_diff = f_diff;
+		this.f_int = f_int;
+		this.p = new vector(3*n);
+	}//ann
+	
+	public double response(double x){
+		//return the response of the network to the input signal x
+		double Fp = 0;
+		//Fp(x) = ∑i f((x-ai)/bi)*wi
+		for(int i=0; i<n; i++){
+			double a = p[3*i+0];
+			double b = p[3*i+1];
+			double w = p[3*i+2];
+			Fp += f((x-a)/b)*w;
+		}
+		return Fp;
+	}//response
+	
+	public double response_diff(double x){
+		double Fp_diff = 0;
+		for(int i=0; i<n; i++){
+			double a = p[3*i+0];
+			double b = p[3*i+1];
+			double w = p[3*i+2];
+			Fp_diff += f_diff((x-a)/b)*w;
+		}
+		return Fp_diff;
+	}//response_diff
+
+	public double response_int(double x){
+		double Fp_int = 0;
+		for(int i=0; i<n; i++){
+			double a = p[3*i+0];
+			double b = p[3*i+1];
+			double w = p[3*i+2];
+			Fp_int += f_int((x-a)/b)*w;
+		}
+		return Fp_int;
+	}//response_int
+
+
+	public void train(vector x, vector y){
+		//Cost function: C(p) = ∑k=1..N (Fp(xk) - yk)²
+		Func<vector,double> cost_function  = C => {
+			p = C;
+			double s = 0;
+			for(int i=0; i<x.size; i++){
+				s += Pow(response(x[i])-y[i],2);
+			}
+			return s/x.size;
+		}; //Func
+		vector v = p;
+		vector result = mini.qnewton(cost_function, v, 1e-3);
+		p = result;
+	}//train
+	
+	
+
+}//class
---
+++ homework/ann/B/main.cs
@@ -0,0 +1,37 @@
+using System;
+using static System.Console;
+using static System.Math;
+
+public class main{
+	public static void Main(){
+		Func<double,double> gw = x => x*Exp(-x*x); //Gaussian wavelet
+		Func<double,double> gw_diff = x => (1-2*x*x)*Exp(-x*x);
+		Func<double,double> gw_int = x => -Exp(-x*x)/2;
+		Func<double,double> g = x => Cos(5*x-1)*Exp(-x*x); //test function
+
+
+		int n = 5; //Hidden neurons
+		ann network = new ann(n, gw, gw_diff, gw_int);//constructing the network
+		double a = -1, b = 1; //interval
+		int m = 20; //number of points
+		vector xs = new vector(m);
+		vector ys = new vector(m);
+		for(int i=0; i<xs.size; i++){ //making x- and y-points
+			xs[i] = a+(b-a)*i/(m-1);
+			ys[i] = g(xs[i]);
+			WriteLine($"{xs[i]} {ys[i]}");
+		}
+		WriteLine("\n");
+		for(int i=0; i<network.n; i++){
+			network.p[3*i+0] = a+(b-a)*i/(network.n-1);
+			network.p[3*i+1] = 1;
+			network.p[3*i+2] = 1;
+		}
+		network.train(xs, ys);
+		for(double z=a; z<b; z+=1.0/64){
+			WriteLine($"{z} {network.response(z)} {network.response_diff(z)} {network.response_int(z)}");
+		}
+	}//Main
+	
+
+}//class
