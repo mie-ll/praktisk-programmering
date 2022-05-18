91c3d7b9653771e771d8ace2d849a3dd25f52ded
Mie Louise Lauridsen <mie-lauridsen@live.dk>
2022-05-18 10:42:36 +0200

    Review of exercises
    Signed-off-by: Mie Louise Lauridsen <mie-lauridsen@live.dk>

--- exercises/math/main.cs
+++ exercises/math/main.cs
@@ -2,14 +2,14 @@ using static System.Math;
 public static class math {
 	static void Main(){
 		double sqrt2=Sqrt(2.0);
-		System.Console.Write($"sqrt(2) = {sqrt2}\n");
-		System.Console.Write($"sqrt2*sqrt2 = {sqrt2*sqrt2}\n");
+		System.Console.Write($"sqrt(2) = {sqrt2}, should be 1.41\n");
+		System.Console.Write($"sqrt2*sqrt2 = {sqrt2*sqrt2}, , should be 2\n");
 		
 		double exppi=Exp(PI);
-		System.Console.Write($"exp(pi) = {exppi}\n");
+		System.Console.Write($"exp(pi) = {exppi}, should be 23.14\n");
 
 		double pie=Pow(PI,E);
-		System.Console.Write($"pi to the power of e = {pie}\n");
+		System.Console.Write($"pi to the power of e = {pie}, should be 22.45\n");
 	}
 
 }
